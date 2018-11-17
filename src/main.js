e526856ae9d479bf2b1c3b008a7cc786bebb9198
coolong <MacLaptop@qq.com>
2018-11-17 22:24:53 +0800

    Project ready

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import fastClick from 'fastclick'
+import './assets/css/reset.css'
+import './assets/css/border.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
