8b446f3ec80d01c68a0683af33a5435601b841fa
dougzhaug <doug@spen.vip>
2018-11-22 18:37:36 +0800

    'index-swiper'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
         <HomeHeader></HomeHeader>
+        <HomeSwiper></HomeSwiper>
+        <div>dddedd</div>
     </div>
 </template>
 
 <script>
     import HomeHeader from './components/HomeHeader'
+    import HomeSwiper from './components/HomeSwiper'
     export default {
         name: "Home",
         components: {
-            HomeHeader
+            HomeHeader,
+            HomeSwiper
         }
     }
 </script>
---
+++ src/views/home/components/HomeSwiper.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" alt="">
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+            <!--<div class="swiper-button-prev" slot="button-prev"></div>-->
+            <!--<div class="swiper-button-next" slot="button-next"></div>-->
+            <!--<div class="swiper-scrollbar"   slot="scrollbar"></div>-->
+        </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeSwiper",
+        data(){
+            return {
+                swiperOption: {
+                    pagination: '.swiper-pagination',
+                    loop: true
+                },
+                swiperList: [
+                    {
+                        id:'001',
+                        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/7c/8e5c4ab8ee8b7402.jpg_750x200_dd7a38dd.jpg'
+                    },
+                    {
+                        id:'002',
+                        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/12/856f100069809e02.jpg_750x200_e3485a2b.jpg'
+                    },
+                    {
+                        id:'003',
+                        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/4d/6706bf772c05ce02.jpg_750x200_773e75f0.jpg'
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background #fff
+    .wrapper
+        overflow hidden
+        width 100%
+        height 0
+        padding-bottom 26.66%
+        background #eee
+        .swiper-img
+            width 100%
+</style>
