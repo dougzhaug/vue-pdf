96a45462a35dd50d5a2236266f5094a92a68ca6f
coolong <MacLaptop@qq.com>
2018-11-24 23:48:57 +0800

    add city list and aplhabet

--- src/views/city/City.vue
+++ src/views/city/City.vue
@@ -2,17 +2,23 @@
    <div>
       <CityHeader></CityHeader>
       <CitySearch></CitySearch>
+      <CityList></CityList>
+      <CityAlphabet></CityAlphabet>
    </div>
 </template>
 
 <script>
     import CityHeader from './components/CityHeader'
     import CitySearch from './components/CitySearch'
+    import CityList from './components/CityList'
+    import CityAlphabet from './components/CityAlphabet'
     export default {
         name: "City",
         components:{
             CityHeader,
-            CitySearch
+            CitySearch,
+            CityList,
+            CityAlphabet
         }
     }
 </script>
