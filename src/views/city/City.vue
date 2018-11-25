45d9e8fc55142216e5058de6f270f54f0980f63d
coolong <MacLaptop@qq.com>
2018-11-25 22:58:31 +0800

    add city search logic

--- src/views/city/City.vue
+++ src/views/city/City.vue
@@ -1,7 +1,7 @@
 <template>
    <div>
       <CityHeader></CityHeader>
-      <CitySearch></CitySearch>
+      <CitySearch :cities="cities"></CitySearch>
       <CityList
               :city="city"
               :hotCities="hotCities"
