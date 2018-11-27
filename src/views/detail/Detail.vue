8d58f86493129d3bc211ad233c807eb06109c299
coolong <MacLaptop@qq.com>
2018-11-27 20:08:06 +0800

    add detail list

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,6 +2,7 @@
     <div>
         <DetailBanner></DetailBanner>
         <DetailHeader></DetailHeader>
+        <DetailList :list="list"></DetailList>
         <div class="content"></div>
     </div>
 </template>
@@ -9,15 +10,47 @@
 <script>
     import DetailBanner from './components/DetailBanner'
     import DetailHeader from './components/DetailHeader'
+    import DetailList from './components/DetailList'
     export default {
         name: "Detail",
         data () {
             return {
+                list:[
+                    {
+                        title:'成人票',
+                        children:[
+                            {
+                                title:'成人三连票'
+                            },
+                            {
+                                title:'成人五连票',
+                                children:[
+                                    {
+                                        title:'成人五连票-京东店'
+                                    },
+                                    {
+                                        title:'成人五连票-田园店'
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        title:'儿童票'
+                    },
+                    {
+                        title:'学生票'
+                    },
+                    {
+                        title:'折扣票'
+                    }
+                ]
             }
         },
         components:{
             DetailBanner,
-            DetailHeader
+            DetailHeader,
+            DetailList
         }
     }
 </script>
